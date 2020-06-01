f1bd64c38254af68439c2bfe96f9fc4582a928f1
antekpapieski <maciejpapiewski9@gmail.com>
2020-06-01 15:02:43 +0200

    add conditional parsing of message to JSON based on 'isBinary' parameter

--- src/ws/middlewares/parseJsonMessage.ts
+++ src/ws/middlewares/parseJsonMessage.ts
@@ -2,6 +2,10 @@ import { WebSocket } from "uWebSockets.js";
 import { WebsocketHandlerContext } from "../carousel";
 
 export default async (ws: WebSocket, message: ArrayBuffer, isBinary: boolean, context: WebsocketHandlerContext) => {
+
+    if(isBinary)
+        return;
+
     const parsedMessage = JSON.parse( Buffer.from( message ).toString()  )
 
     context["message"] = parsedMessage;
