690c0ddfdcd83d1e4c8f077e3bdac77741322038
antekpapieski <maciejpapiewski9@gmail.com>
2020-06-16 19:57:02 +0200

    fix: missing export of extractheaders middleware (http)

--- src/http/middlewares/index.ts
+++ src/http/middlewares/index.ts
@@ -2,3 +2,4 @@ export { default as cors } from "./cors";
 export { default as parseBody } from "./parseBody";
 export { default as parseCookies } from "./parseCookies";
 export { default as parseParams } from "./parseParams";
+export { default as extractHeaders } from "./extractHeaders";
